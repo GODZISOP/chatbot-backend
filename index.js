6418a02429defa680387b968421360dce76df6fa
GODZISOP <appointmentstudio@gmail.com>
2025-06-15 15:20:14 +0500

    s

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const PORT = 5000;  // Changed to port 5000
 
 // Middleware
 app.use(cors({
-  origin: 'https://chatbot-three-eta-74.vercel.app'
+  origin: ['https://chatbot-three-eta-74.vercel.app', 'https://chatbot-c23f.vercel.app/'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
 }));
+
 
 app.use(express.json());
 app.get('/', (req, res) => {
